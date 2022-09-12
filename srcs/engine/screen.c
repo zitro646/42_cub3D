a476301edc422f478e6e5eb644ec18368aed875b
Pablo Otero <potero91@gmail.com>
2022-09-12 15:55:39 +0200

    colors

--- srcs/engine/screen.c
+++ srcs/engine/screen.c
@@ -6,7 +6,7 @@
 /*   By: potero-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 12:07:53 by potero-d          #+#    #+#             */
-/*   Updated: 2022/09/12 15:42:37 by potero-d         ###   ########.fr       */
+/*   Updated: 2022/09/12 15:55:20 by potero-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 void	screen_game(t_game *game)
 {
-//	int		r;
-//	double	proportion;
 	int		x;
 	int		y;
 	int		start;
@@ -29,23 +27,21 @@ void	screen_game(t_game *game)
 		y = 0;
 		start = middle - (game->ray[x].wall / 2);
 		end = start + (int) game->ray[x].wall;
-	//	if (x == 0 || x == 512 || x == 1023)
-	//		printf("y_[%d]: %d: %d\n", x, start, end);
-		if ( x % 4 == 0)
+		if ( x % 2 == 0)
 		{
 			while (y < start)
 			{
-			mlx_pixel_put(game->mlx.mlx, game->mlx.screen, x, y, 0x000000);
+			mlx_pixel_put(game->mlx.mlx, game->mlx.screen, x, y, 0xFF0000);
 			y++;
 			}
 			while (y < end)
 			{
-				mlx_pixel_put(game->mlx.mlx, game->mlx.screen, x, y, 0x4B0082);
+				mlx_pixel_put(game->mlx.mlx, game->mlx.screen, x, y, 0xFFFF00);
 				y++;
 			}
 			while (y < game->height)
 			{
-				mlx_pixel_put(game->mlx.mlx, game->mlx.screen, x, y, 0xFF0000);
+				mlx_pixel_put(game->mlx.mlx, game->mlx.screen, x, y, 0x4B0082);
 				y++;
 			}
 		}
